d39bccb7ecc3017bef1b4188b71151073350494f
Mitch Bodmer <mibodmer@microsoft.com>
2017-03-10 11:06:28 -0800

    WithCorrelationLogContextGuid_does_not_return_a_log_event_without_a_correlation_id

--- serilog-utilities-concurrent-correlator-tests/EnumerableOfLogEventExtensionsTests.cs
+++ serilog-utilities-concurrent-correlator-tests/EnumerableOfLogEventExtensionsTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using FluentAssertions;
 using Serilog.Events;
+using Serilog.Parsing;
 using Xunit;
 
 namespace Serilog.Utilities.ConcurrentCorrelator.Tests
@@ -12,6 +13,19 @@ namespace Serilog.Utilities.ConcurrentCorrelator.Tests
         public void WithCorrelationLogContextGuid_returns_empty_if_no_log_events_have_been_logged()
         {
             new List<LogEvent>().WithCorrelationLogContextGuid(Guid.NewGuid()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void WithCorrelationLogContextGuid_does_not_return_a_log_event_without_a_correlation_id()
+        {
+            var logEventWithoutCorrelationId = new LogEvent(DateTimeOffset.Now, LogEventLevel.Information, null,
+                new MessageTemplate("Message template.", new List<MessageTemplateToken>()),
+                new List<LogEventProperty>());
+
+            new List<LogEvent>
+            {
+                logEventWithoutCorrelationId
+            }.WithCorrelationLogContextGuid(Guid.NewGuid()).Should().BeEmpty();
         }
     }
 }
