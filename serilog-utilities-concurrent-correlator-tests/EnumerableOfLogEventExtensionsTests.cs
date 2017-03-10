87fa056a580337ebea3b3d4ee078f4a3164ed9a8
Mitch Bodmer <mibodmer@microsoft.com>
2017-03-10 11:01:37 -0800

    WithCorrelationLogContextGuid_returns_empty_if_no_log_events_have_been_logged

--- serilog-utilities-concurrent-correlator-tests/EnumerableOfLogEventExtensionsTests.cs
+++ serilog-utilities-concurrent-correlator-tests/EnumerableOfLogEventExtensionsTests.cs
@@ -1,9 +1,17 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Serilog.Events;
 using Xunit;
 
 namespace Serilog.Utilities.ConcurrentCorrelator.Tests
 {
     public class EnumerableOfLogEventExtensionsTests
     {
+        [Fact]
+        public void WithCorrelationLogContextGuid_returns_empty_if_no_log_events_have_been_logged()
+        {
+            new List<LogEvent>().WithCorrelationLogContextGuid(Guid.NewGuid()).Should().BeEmpty();
+        }
     }
 }
