49846c231cc027979fa8bbb80aac79518fd0b838
Mitch Bodmer <mibodmer@microsoft.com>
2017-03-10 11:24:16 -0800

    WithCorrelationLogContextGuid_returns_empty_if_no_log_events_have_been_logged_with_the_correlation_guid

--- serilog-utilities-concurrent-correlator-tests/EnumerableOfLogEventExtensionsTests.cs
+++ serilog-utilities-concurrent-correlator-tests/EnumerableOfLogEventExtensionsTests.cs
@@ -9,6 +9,27 @@ namespace Serilog.Utilities.ConcurrentCorrelator.Tests
 {
     public class EnumerableOfLogEventExtensionsTests
     {
+        private LogEvent GetLogEventWithoutCorrelationGuid()
+        {
+            return new LogEvent(DateTimeOffset.Now,
+                LogEventLevel.Information, null,
+                new MessageTemplate("Message template.", new List<MessageTemplateToken>()),
+                new List<LogEventProperty>());
+        }
+
+        private LogEvent GetLogEventWithCorrelationGuid(Guid correlationGuid)
+        {
+            var logEventWithCorrelationGuid = new LogEvent(DateTimeOffset.Now,
+                LogEventLevel.Information, null,
+                new MessageTemplate("Message template.", new List<MessageTemplateToken>()),
+                new List<LogEventProperty>
+                {
+                    new LogEventProperty("CorrelationGuid", new ScalarValue(correlationGuid))
+                });
+
+            return logEventWithCorrelationGuid;
+        }
+
         [Fact]
         public void WithCorrelationLogContextGuid_returns_empty_if_no_log_events_have_been_logged()
         {
@@ -16,15 +37,21 @@ namespace Serilog.Utilities.ConcurrentCorrelator.Tests
         }
 
         [Fact]
-        public void WithCorrelationLogContextGuid_does_not_return_a_log_event_without_a_correlation_guid()
+        public void
+            WithCorrelationLogContextGuid_returns_empty_if_no_log_events_have_been_logged_with_the_correlation_guid()
         {
-            var logEventWithoutCorrelationGuid = new LogEvent(DateTimeOffset.Now, LogEventLevel.Information, null,
-                new MessageTemplate("Message template.", new List<MessageTemplateToken>()),
-                new List<LogEventProperty>());
-
             new List<LogEvent>
             {
-                logEventWithoutCorrelationGuid
+                GetLogEventWithCorrelationGuid(Guid.NewGuid())
+            }.WithCorrelationLogContextGuid(Guid.NewGuid()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void WithCorrelationLogContextGuid_does_not_return_a_log_event_without_a_correlation_guid()
+        {
+            new List<LogEvent>
+            {
+                GetLogEventWithoutCorrelationGuid()
             }.WithCorrelationLogContextGuid(Guid.NewGuid()).Should().BeEmpty();
         }
     }
