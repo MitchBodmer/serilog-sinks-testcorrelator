31367b0f2627a130f9073e860b5b853bb4de330d
Mitch Bodmer <mibodmer@microsoft.com>
2017-03-10 18:01:16 -0800

    When_the_module_is_loaded_the_global_logger_is_not_a_SilentLogger

---
+++ serilog-utilities-concurrent-correlator-tests/ModuleInitializerTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Serilog.Utilities.ConcurrentCorrelator.Tests
+{
+    public class ModuleInitializerTests
+    {
+        [Fact]
+        public void When_the_module_is_loaded_the_global_logger_is_not_a_SilentLogger()
+        {
+            //Force the module to load.
+            using (new CorrelationLogContext()) 
+            {
+                Log.Logger.GetType().FullName.Should().NotBe("Serilog.Core.Pipeline.SilentLogger");
+            }
+        }
+    }
+}
