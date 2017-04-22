836f6c22301afa87c8bcb4df17a76577f18afc8c
Mitch Bodmer <mibodmer@microsoft.com>
2017-04-22 13:00:57 -0700

    Make AssemblyInfos consistent and use copyright information from them in nuspec.

--- serilog-utilities-concurrent-correlator-tests/Properties/AssemblyInfo.cs
+++ serilog-utilities-concurrent-correlator-tests/Properties/AssemblyInfo.cs
@@ -1,10 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("serilog-utilities-concurrent-correlator-tests")]
-[assembly: AssemblyProduct("serilog-utilities-concurrent-correlator-tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTitle("Serilog.Utilities.ConcurrentCorrelator.Tests")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Serilog.Utilities.ConcurrentCorrelator.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d7af91a0-2039-4c46-b0e4-f9d3b3091f89")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- serilog-utilities-concurrent-correlator/Properties/AssemblyInfo.cs
+++ serilog-utilities-concurrent-correlator/Properties/AssemblyInfo.cs
@@ -2,12 +2,11 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Serilog.Utilities.ConcurrentCorrelator")]
-[assembly: AssemblyDescription("A small library for unit testing Serilog log events in concurrent testing frameworks.")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("A library for unit testing Serilog log events in concurrent testing frameworks.")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Serilog.Utilities.ConcurrentCorrelator")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bf409844-1ca5-4817-8906-cb1c91f4948a")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
