557b6b39fc6f302b96ce37406cbba3aea4748b92
Mitch Bodmer <mitchbodmer@outlook.com>
2017-05-13 19:00:00 -0700

    Remove unnecessary usings.

--- serilog-utilities-concurrent-correlator-tests/TestSerilogLogEventsTests.cs
+++ serilog-utilities-concurrent-correlator-tests/TestSerilogLogEventsTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using Serilog.Events;
-using Serilog.Parsing;
 using Xunit;
 
 namespace Serilog.Utilities.ConcurrentCorrelator.Tests
