8516474bc42fe2f8a9d5f635519ed34c79e68211
Mitch Bodmer <mitchbodmer@outlook.com>
2017-05-14 16:59:57 -0700

    A_TestSerilogEventsNotConfiguredException_can_be_serialized_and_deserialized

--- src/serilog-utilities-concurrent-correlator/TestSerilogEventsNotConfiguredException.cs
+++ src/serilog-utilities-concurrent-correlator/TestSerilogEventsNotConfiguredException.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Serilog.Utilities.ConcurrentCorrelator
 {
+    [Serializable]
     public class TestSerilogEventsNotConfiguredException : Exception
     {
         public TestSerilogEventsNotConfiguredException() :
             base(
                 $"Serilog's global logger has not been configured for testing. This can either be because you did not call {nameof(TestSerilogLogEvents.ConfigureGlobalLoggerForTesting)}, or because other code has overwritten {nameof(Serilog.Log.Logger)} since you did.")
         { }
+
+        protected TestSerilogEventsNotConfiguredException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
--- test/serilog-utilities-concurrent-correlator-tests/TestSerilogEventsNotConfiguredExceptionTests.cs
+++ test/serilog-utilities-concurrent-correlator-tests/TestSerilogEventsNotConfiguredExceptionTests.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using FluentAssertions;
 using Xunit;
 
 namespace Serilog.Utilities.ConcurrentCorrelator.Tests
@@ -9,6 +12,23 @@ namespace Serilog.Utilities.ConcurrentCorrelator.Tests
         public void A_TestSerilogEventsNotConfiguredException_should_have_the_correct_message()
         {
             new TestSerilogEventsNotConfiguredException().Message.Should().Be("Serilog's global logger has not been configured for testing. This can either be because you did not call ConfigureGlobalLoggerForTesting, or because other code has overwritten Logger since you did.");
+        }
+
+        [Fact]
+        public void A_TestSerilogEventsNotConfiguredException_can_be_serialized_and_deserialized()
+        {
+            var buffer = new byte[4096];
+
+            var serializationStream = new MemoryStream(buffer);
+            var deserializationStream = new MemoryStream(buffer);
+
+            var formatter = new BinaryFormatter();
+
+            var exception = new TestSerilogEventsNotConfiguredException();
+
+            formatter.Serialize(serializationStream, exception);
+
+            formatter.Deserialize(deserializationStream).Should().BeOfType<TestSerilogEventsNotConfiguredException>();
         }
     }
 }
