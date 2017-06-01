221b82535a19f28d5e3cc6d43cf1b3694ce3c670
Mitch Bodmer <mitchbodmer@outlook.com>
2017-05-31 23:57:29 -0700

    Move AssemblyInfo into csproj. Rename project files.

--- test/serilog-utilities-concurrent-correlator-tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Serilog.Utilities.ConcurrentCorrelator.Tests")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Serilog.Utilities.ConcurrentCorrelator.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.0.0")]
