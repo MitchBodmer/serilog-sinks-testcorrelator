20b11cdd461528fc2bc9f77f6c2a900c78fc31d5
Mitch Bodmer <mitchbodmer@outlook.com>
2017-06-17 21:03:42 -0700

    Update exception message.

--- src/SerilogTestCorrelation/GlobalLoggerNotConfiguredForTestCorrelationException.cs
+++ src/SerilogTestCorrelation/GlobalLoggerNotConfiguredForTestCorrelationException.cs
@@ -7,7 +7,7 @@ namespace SerilogTestCorrelation
     {
         public GlobalLoggerNotConfiguredForTestCorrelationException() :
             base(
-                $"Serilog's global logger has not been configured for test correlation. This can either be because you did not call {nameof(SerilogTestCorrelator.ConfigureGlobalLoggerForTestCorrelation)}, or because other code has overwritten {nameof(Log.Logger)} since you did.")
+                $"Serilog's global logger has not been configured for test correlation. The {nameof(SerilogTestCorrelator)} will not be able to collect LogEvents. This may be because you did not call {nameof(SerilogTestCorrelator)}.{nameof(SerilogTestCorrelator.ConfigureGlobalLoggerForTestCorrelation)}(), or because other code has overwritten {nameof(Serilog)}.{nameof(Log)}.{nameof(Log.Logger)} since you did.")
         {
         }
     }
