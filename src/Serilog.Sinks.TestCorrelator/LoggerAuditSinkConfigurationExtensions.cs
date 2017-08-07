436fb272e5dc0b0569ab4995e353f34d8b85e9ad
Mitch Bodmer <mitchbodmer@outlook.com>
2017-08-06 19:18:54 -0700

    Clean up comments.

--- src/Serilog.Sinks.TestCorrelator/LoggerAuditSinkConfigurationExtensions.cs
+++ src/Serilog.Sinks.TestCorrelator/LoggerAuditSinkConfigurationExtensions.cs
@@ -6,17 +6,17 @@ using Serilog.Sinks.TestCorrelator;
 namespace Serilog
 {
     /// <summary>
-    /// Adds the <see cref="LoggerConfiguration.AuditTo"/>.<see cref="TestCorrelator"/>() extension method to <see cref="LoggerAuditSinkConfiguration"/>.
+    /// Adds the AuditTo.TestCorrelator extension method to a LoggerAuditSinkConfiguration.
     /// </summary>
     public static class LoggerAuditSinkConfigurationExtensions
     {
         /// <summary>
         /// Adds a <see cref="TestCorrelatorSink"/> to audit to.
         /// </summary>
-        /// <param name="loggerAuditSinkConfiguration">The <see cref="LoggerAuditSinkConfiguration"/> to audit to.</param>
-        /// <param name="restrictedToMinimumLevel">The minimum <see cref="LogEventLevel"/> for events passed to the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
-        /// <param name="levelSwitch">A switch allowing the minimum <see cref="LogEventLevel"/> to be changed at runtime.</param>
-        /// <returns>A modified <see cref="LoggerConfiguration"/> allowing configuration to continue.</returns>
+        /// <param name="loggerAuditSinkConfiguration">The LoggerAuditSinkConfiguration to audit to.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum LogEventLevel for events passed to the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the minimum LogEventLevel to be changed at runtime.</param>
+        /// <returns>A modified LoggerConfiguration allowing configuration to continue.</returns>
         public static LoggerConfiguration TestCorrelator(
             this LoggerAuditSinkConfiguration loggerAuditSinkConfiguration,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
--- src/Serilog.Sinks.TestCorrelator/LoggerSinkConfigurationExtensions.cs
+++ src/Serilog.Sinks.TestCorrelator/LoggerSinkConfigurationExtensions.cs
@@ -6,17 +6,17 @@ using Serilog.Sinks.TestCorrelator;
 namespace Serilog
 {
     /// <summary>
-    /// Adds the <see cref="LoggerConfiguration.WriteTo"/>.<see cref="TestCorrelator"/>() extension method to <see cref="LoggerSinkConfiguration"/>.
+    /// Adds the WriteTo.TestCorrelator extension method to LoggerSinkConfiguration.
     /// </summary>
     public static class LoggerSinkConfigurationExtensions
     {
         /// <summary>
         /// Adds a <see cref="TestCorrelatorSink"/> to write to.
         /// </summary>
-        /// <param name="loggerSinkConfiguration">The <see cref="LoggerSinkConfiguration"/> to write to.</param>
-        /// <param name="restrictedToMinimumLevel">The minimum <see cref="LogEventLevel"/> for events passed to the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
-        /// <param name="levelSwitch">A switch allowing the minimum <see cref="LogEventLevel"/> to be changed at runtime.</param>
-        /// <returns>A modified <see cref="LoggerConfiguration"/> allowing configuration to continue.</returns>
+        /// <param name="loggerSinkConfiguration">The LoggerSinkConfiguration to write to.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum LogEventLevel for events passed to the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the minimum LogEventLevel to be changed at runtime.</param>
+        /// <returns>A modified LoggerConfiguration allowing configuration to continue.</returns>
         public static LoggerConfiguration TestCorrelator(
             this LoggerSinkConfiguration loggerSinkConfiguration,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
